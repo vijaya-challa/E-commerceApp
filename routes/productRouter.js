19f58070253f35eadb195c9c70b5e0df48d84aae
Vijaya Challa <vijayajay1814@gmail.com>
2023-03-21 12:39:46 +0100

    added get method to find by id

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,7 +4,7 @@ import createError from 'http-errors'
 
 const productRouter = express.Router()
 
-
+// get all Products
 productRouter.get('/', async (req, res, next) => {
 
   try {
@@ -16,6 +16,7 @@ productRouter.get('/', async (req, res, next) => {
 
 })
 
+// create new Product
 productRouter.post('/', async (req, res, next) => {
 
   try {
@@ -28,11 +29,24 @@ productRouter.post('/', async (req, res, next) => {
 
 })
 
-// productRouter.get('/id', (req,res,next)=>{
+
+// get product by id
+productRouter.get('/:id', async (req, res, next) => {
+  try {
+
+    const id = req.params.id
+    const getProductById = await ProductModel.findById(id)
+    console.log(getProductById)
+    res.send(getProductById)
+  } catch (error) {
+    next(createError(404, error.message))
+  }
 
 
-// })
+})
 
+
+// update specific product
 
 
 
